7bf63c43fdc41163296c09ffc4b1340fd059aaa2
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-06-18 02:49:32 -0700

    set up and style static page content

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const afternoon = "https://i.ibb.co/3mThcXc/afternoon.jpg";
+const morning = "https://i.ibb.co/7vDLJFb/morning.jpg";
+const evening = "https://i.ibb.co/924T2Wv/night.jpg";
+
+// DOM Elements
+const time = document.getElementById("time"),
+  greeting = document.getElementById("greeting"),
+  name = document.getElementById("name"),
+  focus = document.getElementById("focus");
+
+// Show time
