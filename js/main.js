2fd5c4687a6a1fa92570c182a9529c9016b795b8
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-06-18 03:23:10 -0700

    make time change every second, change background and greeting based on time of day

--- js/main.js
+++ js/main.js
@@ -9,3 +9,55 @@ const time = document.getElementById("time"),
   focus = document.getElementById("focus");
 
 // Show time
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+  // Set AM or PM
+  const amPm = hour >= 12 ? "PM" : "AM";
+
+  // 12hr Format
+  hour = hour % 12 || 12;
+
+  // Output time
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )} ${amPm}`;
+
+  setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+// Set Background and Greeting
+function setBgGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    // Morning
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+    greeting.textContent = "Good Morning";
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+    greeting.textContent = "Good Afternoon";
+  } else {
+    // Evening
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/924T2Wv/night.jpg')";
+    greeting.textContent = "Good Evening";
+    document.body.style.color = "white";
+  }
+}
+
+// Run
+showTime();
+setBgGreet();
