d6312b3d5b0f0ee143d4b695b73d87bee4b5a826
Nizamuddin Sulieman <kuntau17@gmail.com>
2020-04-07 06:20:44 +0800

    Migrated from codepen
    Convert mdi back to fontawesome for the awesome duotone icons

---
+++ script.js
@@ -0,0 +1,17 @@
+const sideNav = [
+  "Dashboard",
+  "Reports",
+  "Charts",
+  "Produts",
+  "Inbox",
+  "Settings"
+];
+
+const topNavLeft = ["Home", "Name"];
+
+const topNavRight = ["Home", "Kuntau"];
+
+var app = new Vue({
+  el: "#app",
+  data: {}
+});
