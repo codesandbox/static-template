1be19de0bcb855b786da543e3061ca720fc567fe
Daniel Leyva Cortés <57109779+grafeno30@users.noreply.github.com>
2020-06-21 19:50:48 +0200

    Hay problemas ocn la ventana modal, no me reconoce jquery ,etc...

--- js/acciones.js
+++ js/acciones.js
@@ -1,19 +1,46 @@
-function principioPartida() {
-  var max = 7;
-  var min = 0;
+$(document).ready(function() {
+  var segundosIniciales = 0;
 
-  var caballoi = Math.floor(Math.random() * (max - min + 1)) + min;
-  var caballoj = Math.floor(Math.random() * (max - min + 1)) + min;
+  //Inicia el cronómetro
+  function iniciaCronometro() {
+    document.getElementById("#cronometro").innerHTML = "Tiempo <br/> 00:00";
+    segundosIniciales = new Date().getSeconds();
+  }
 
-  var nombre_celda = "c" + caballoi + caballoj;
+  /*
+  //actualiza el cronómetro
+  function actualizaCronometro() {
+    var segundosTranscurridos = new Date().getSeconds() - segundosIniciales;
+    var segundosTranscurridosCadena = segundosTranscurridos.toString();
+    document.getElementById("#cronometro").innerHTML =
+      "Tiempo <br/>" + segundosTranscurridosCadena;
+  }
+  */
+  function principioPartida() {
+    //hacemos primero que se visualice la ventana modoal
+    //document.getElementById("#ventanaModal").modal("show");
+    $("#ventanaModal").modal("show");
 
-  var celda = document.getElementById(nombre_celda);
+    iniciaCronometro();
 
-  console.log("Posición caballo " + nombre_celda);
+    var max = 7;
+    var min = 0;
 
-  celda.style.backgroundImage = "url('../img/caballo.png')";
-  celda.style.backgroundRepeat = "no-repeat";
-  celda.style.backgroundPosition = "center";
-}
+    var caballoi = Math.floor(Math.random() * (max - min + 1)) + min;
+    var caballoj = Math.floor(Math.random() * (max - min + 1)) + min;
 
-principioPartida();
+    var nombre_celda = "c" + caballoi + caballoj;
+
+    var celda = document.getElementById(nombre_celda);
+
+    console.log("Posición caballo " + nombre_celda);
+
+    celda.style.backgroundImage = "url('../img/caballo.png')";
+    celda.style.backgroundRepeat = "no-repeat";
+    celda.style.backgroundPosition = "center";
+  }
+
+  //Inciamos un timer para que cada segundo actualice el cronómetro
+  //setInterval(actualizaCronometro, 1000);
+  principioPartida();
+});
