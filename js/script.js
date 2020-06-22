0060c17d2f19e85c32f7dd6d814b07ed03c19af6
Mai <54549751+MvRyde@users.noreply.github.com>
2020-06-21 20:59:57 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("JS Loaded!");
+
+document.documentElement.style.setProperty("--animate-duration", "10s");
+
+// All animations will take half the time to accomplish
+document.documentElement.style.setProperty("--animate-duration", "5s");
