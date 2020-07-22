c171824a0d5a829b32b4133ca44579251284b144
Jerome Biggins <jeromeebiggins@gmail.com>
2020-07-22 12:57:07 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementsByClassName("item");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
