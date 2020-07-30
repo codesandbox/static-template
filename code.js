2d3b827db7f317bf4f7a243443c5b4c0e4cb9936
KAULD1 <44489019+KAULD1@users.noreply.github.com>
2020-07-30 11:01:53 +1000

    Online Version Not connected via Xampp

--- code.js
+++ code.js
@@ -2,34 +2,39 @@
 var guess = 0; // the user's guess
 var target = 0; // number to be guessed
 var guesses = 0; // count the guesses
-var message = ''; // message at top of page
-var guessbtn = ''; // label for the button
+var message = ""; // message at top of page
+var guessbtn = ""; // label for the button
 
 function newgame() {
-   // initialise variables for a new game
-   guess = 0;
-   target = Math.floor(Math.random()*100)+1;
-   guesses = 0;
-   message = 'Guess the number!';
-   guessbtn = 'Take a guess';
+  // initialise variables for a new game
+  guess = 0;
+  target = Math.floor(Math.random() * 100) + 1;
+  guesses = 0;
+  message = "Guess the number between 0-100!";
+  guessbtn = "Take a guess";
 }
 
 function go() {
-   // increment the number of guesses
-   guesses++;
-   // retrieve the guess from the form
-   guess = document.getElementById("guess").value;
-   // check if the guess is too large, small or correct
-   if (guess > target) {
-      message = 'Your guess of ' + guess + ' was too big.';
-   } else if (guess < target) {
-      message = 'Your guess of ' + guess + ' was too small.';
-   } else {
-      message = 'Correct! You took ' + guesses + ' guesses.';
-      guessbtn = 'High scores';
-      document.getElementById("guessbtn").setAttribute( "onClick", "window.location.href='highscores.php?s="+guesses+"'");
-   }
-   // modify the display text and button value if required
-   document.getElementById("guessbtn").value = guessbtn;
-   document.getElementById("displaytext").innerText = message;
+  // increment the number of guesses
+  guesses++;
+  // retrieve the guess from the form
+  guess = document.getElementById("guess").value;
+  // check if the guess is too large, small or correct
+  if (guess > target) {
+    message = "Your guess of " + guess + " was too big.";
+  } else if (guess < target) {
+    message = "Your guess of " + guess + " was too small.";
+  } else {
+    message = "Correct! You took " + guesses + " guesses.";
+    guessbtn = "High scores";
+    document
+      .getElementById("guessbtn")
+      .setAttribute(
+        "onClick",
+        "window.location.href='highscores.php?s=" + guesses + "'"
+      );
+  }
+  // modify the display text and button value if required
+  document.getElementById("guessbtn").value = guessbtn;
+  document.getElementById("displaytext").innerText = message;
 }
