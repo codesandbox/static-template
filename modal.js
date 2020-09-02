75c913e17979de7982fb0e735ad95365363d4d86
prashantguptabro <68388206+prashantguptabro@users.noreply.github.com>
2020-09-02 23:59:26 +0530

    initial commit

---
+++ modal.js
@@ -0,0 +1,10 @@
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
