3521aa821606de2ea3f4196c6b61efa23196c991
Jenniferchan3 <68825434+Jenniferchan3@users.noreply.github.com>
2020-09-24 11:37:41 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const searchButton = document.getElementById("search-button");
+const closeModal = document.getElementById("close-button");
+const submitButton = document.getElementById("submit-button");
+
+searchButton.addEventListener("click", function () {
+  document.getElementById("modal-main").classList.toggle("open");
+});
+
+closeModal.addEventListener("click", function () {
+  document.getElementById("modal-main").classList.toggle("open");
+});
+
+submitButton.addEventListener("click", function () {
+  document.getElementById("results-text").classList.toggle("open");
+  document.getElementById("modal-text").classList.toggle("hide");
+});
