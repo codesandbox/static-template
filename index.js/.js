4a4cc4acfd9555e736a89db49cffe2e3147bc025
DougzybuyDesign <70322945+DougzybuyDesign@users.noreply.github.com>
2020-11-17 12:39:15 +0000

    initial commit

---
+++ index.js/.js
@@ -0,0 +1,10 @@
+import "./app.scss";
+
+document.getElementById("app").innerHTML = `
+<div class="container">
+  <div class="alert alert-warning" role="alert">
+    This is a example how to import custom variable files on sass for the stackoverflow question:
+  </div>
+  <button href="https://stackoverflow.com/questions/52757719/sass-and-bootstrap-issue-with-importing-scss-file/" type="button" class="btn btn-primary">Open Stackoverflow question</button>
+</div>
+`;
