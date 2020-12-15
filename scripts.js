b4b567b39341b7a13d936bc81e97887bae27a4ee
Influify <influify@gmail.com>
2020-12-14 20:39:25 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,43 @@
+/* Makes the shop navigation fly in from the left */
+function showMenu() {
+  document.getElementById("shop").style.left = "0";
+}
+
+/* makes the navigation fly out to the left */
+function hideMenu() {
+  document.getElementById("shop").style.left = "-100%";
+}
+
+// Slideshow scripts
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
