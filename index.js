d7444ffa7befdb91a375092c2dd16847c4008327
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2021-01-20 16:17:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//vehicle object
+//make,name,fuelCapacity
+//functions to be present : startCar, stopCar
+//should check for : isFuelPresent, isEngineStarted, isDifferencialsAligned
+
+// Car car = new Car(args);
+//car startCar() // car started
