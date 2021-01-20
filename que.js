fd9cd28ee03eef41499a70f7004eb5f095631c9a
santhosh <68135777+santhosh456s@users.noreply.github.com>
2021-01-20 17:03:17 +0530

    initial commit

---
+++ que.js
@@ -0,0 +1,41 @@
+let questions = [{
+  num: 1,
+  question: "In Java array are",
+  answer: "Object",
+  options: [
+      "Object Refrence",
+      "Object",
+      "Primitive data type",
+      "none of the above"
+  ],
+}, {
+  num: 2,
+  question: "Which one of the following is valid Statement",
+  answer: "[] c = new char[4];",
+  options: [
+      "char[] c = new char();",
+      "[] c = new char[4];",
+      "char[] c = new char(4);",
+      "char[] c = new char[];"
+  ],
+}, {
+  num: 3,
+  question: "When you pass the array into the method,the method receives _______",
+  answer: "Copy of the array",
+  options: [
+      "refrence of the array",
+      "Copy of the array",
+      "Length of the array",
+      "A copy of the first element"
+  ],
+}, {
+  num: 4,
+  question: "When you pass the array into the method,the method receives _______",
+  answer: "Copy of the array",
+  options: [
+      "refrence of the array",
+      "Copy of the array",
+      "Length of the array",
+      "A copy of the first element"
+  ],
+}];
