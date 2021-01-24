aecef02997c6111f20e18311b9e1f4386845fc84
Eduardo Virgilio <eduardomiguelvirgilio@gmail.com>
2021-01-24 18:55:25 -0300

    Update Water Element

--- js/player.js
+++ js/player.js
@@ -4,7 +4,8 @@ const customPlayer = () => {
   document.querySelector("#btn-play").onclick = (e) => {
     e.preventDefault();
     if (document.querySelector(".video").classList.contains("paused")) {
-      document.querySelector(".video video").stop();
+      document.querySelector(".video video").pause();
+      document.querySelector(".video video").currentTime = 0;
       document.querySelector(".video video").removeAttribute("loop");
       document.querySelector(".video video").removeAttribute("muted");
       document.querySelector(".video video").removeAttribute("autoplay");
@@ -12,13 +13,15 @@ const customPlayer = () => {
       document.querySelector(".video").classList.add("played");
       setTimeout(() => document.querySelector(".video video").play(), 1000);
     } else {
-      document.querySelector(".video video").stop();
+      document.querySelector(".video video").pause();
+      document.querySelector(".video video").currentTime = 0;
       document.querySelector(".video").classList.add("paused");
       document.querySelector(".video").classList.remove("played");
       setTimeout(() => {
         document.querySelector(".video video").toggleAttribute("loop");
         document.querySelector(".video video").toggleAttribute("muted");
         document.querySelector(".video video").toggleAttribute("autoplay");
+        document.querySelector(".video video").play();
       }, 1000);
     }
   };
