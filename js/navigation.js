7943e96d4a7dc0e2b084ad4036ad2a437e4b8eb9
Eduardo Virgilio <eduardomiguelvirgilio@gmail.com>
2021-01-24 19:41:45 -0300

    Update Website Elements

--- js/navigation.js
+++ js/navigation.js
@@ -1,14 +1,11 @@
-window.onload = () => navigationToggler();
-const navigationToggler = () => {
-  document.querySelector("a[href='#menu']").onclick = (e) => {
-    e.preventDefault();
-    debugger;
-    if (document.querySelector("#menu").classList.contains("show")) {
-      document.querySelector("#menu").classList.remove("show");
-      document.querySelector("#menu").classList.add("hide");
-    } else {
-      document.querySelector("#menu").classList.add("show");
-      document.querySelector("#menu").classList.remove("hide");
-    }
-  };
+document.querySelector("a[href='#menu']").onclick = (e) => {
+  e.preventDefault();
+  debugger;
+  if (document.querySelector("#menu").classList.contains("show")) {
+    document.querySelector("#menu").classList.remove("show");
+    document.querySelector("#menu").classList.add("hide");
+  } else {
+    document.querySelector("#menu").classList.add("show");
+    document.querySelector("#menu").classList.remove("hide");
+  }
 };
