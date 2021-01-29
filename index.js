251d916c5dea83d5b9bec948e728994e9f818286
LisaEstebe <74145721+LisaEstebe@users.noreply.github.com>
2021-01-29 09:12:41 +0100

    ConvertisseurFrancs/Euros

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-console.log("bonjour");
+//console.log("bonjour");
 
 let cible1 = document.getElementById("versfrancs");
 
 cible1.addEventListener("click", conversionFrancs);
 
-function conversionFrancs() {
-  console.log("conversion");
+function conversionFrancs(event) {
+  //recuperer la valeur de saisie
+  let valEuro = document.getElementById("valeur").value;
+  //conversion
+  let valFranc = valEuro * 6.55;
+  //resultat
+  document.getElementById("resultat").value = valFranc;
+}
+
+let cible2 = document.getElementById("verseuros");
+
+cible2.addEventListener("click", conversionEuros);
+
+function conversionEuros(event) {
+  //recuperer la valeur de saisie
+  let valFranc = document.getElementById("valeur").value;
+  //conversion
+  let valEuro = valFranc / 6.55;
+  //resultat
+  document.getElementById("resultat").value = valEuro;
 }
