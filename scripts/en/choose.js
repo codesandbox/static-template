95bf0c7729c9b7584d3d2b8f69f59c6e30c78174
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2021-02-14 11:37:22 +0000

    initial commit

---
+++ scripts/en/choose.js
@@ -0,0 +1,35 @@
+var first_page = document.getElementById("mainpage");
+first_page.innerHTML = xyqumygfpqhugpbitsih + nnzobhbzebbfcaatifrx + iceonrtdcxeqgnqpdrrn + zaermurgajocgvoehmnm;
+
+
+function logo(){
+  first_page.innerHTML = "Nothing Here";
+}
+function screenchot(){
+  first_page.innerHTML = nnzobhbzebbfcaatifrx + iceonrtdcxeqgnqpdrrn;
+}
+function ptv(){
+  first_page.innerHTML = xyqumygfpqhugpbitsih + zaermurgajocgvoehmnm;
+}
+
+function monday(){
+  first_page.innerHTML = "Nothing Here";
+}
+function tuesday(){
+  first_page.innerHTML = nnzobhbzebbfcaatifrx;
+}
+function wednesday(){
+  first_page.innerHTML = "Nothing Here";
+}
+function thursday(){
+  first_page.innerHTML = "Nothing Here";
+}
+function friday(){
+  first_page.innerHTML = xyqumygfpqhugpbitsih + iceonrtdcxeqgnqpdrrn + zaermurgajocgvoehmnm;
+}
+function saturday(){
+  first_page.innerHTML = "Nothing Here";
+}
+function sunday(){
+  first_page.innerHTML = "Nothing Here";
+}
---
+++ scripts/en/id.js
@@ -0,0 +1,17 @@
+// Pokémon
+var news_1 = '<a href="';
+var news_2 =
+  '" > <div class="news"> <div class="news_img_figure"> <img class="news_img" src="';
+var news_3 = '" /> </div> <div class="media-body"> <p class="date">';
+var news_4 = '</p> <h3 class="headline">';
+var news_5 = '</h3> <p class="features">';
+var news_6 = "</p></div></div></a>";
+
+
+var xyqumygfpqhugpbitsih = news_1+"/en/pokemon-tv-is-showing-a-collection-of-episodes-featuring-love"+news_2+"https://assets.pokemon.com/assets//cms2/img/watch-pokemon-tv/_tiles/stunts/love/love-169-en.png"+news_3+"12 February 2021"+news_4+"Pokémon TV is showing a collection of episodes featuring love."+news_5+"Images"+news_6;
+
+var nnzobhbzebbfcaatifrx = news_1+"/en/pokemon-journeys-the-series-episode-31-arrives-on-pokemon-tv"+news_2+"https://pokemon-tv-assets.netlify.app/season23/season23_ep31.png"+news_3+"9 February 2021"+news_4+"<em>Pokémon Journeys: The Series</em> episode 31 arrives on Pokémon TV."+news_5+"Screenshots"+news_6;
+
+var iceonrtdcxeqgnqpdrrn = news_1+"/en/season-7-of-pokemon-the-series-is-now-playing-on-pokemon-tv"+news_2+"https://assets.pokemon.com/assets/cms2/img/watch-pokemon-tv/_tiles/season07/ptv-season7-169.jpg"+news_3+"5 February 2021"+news_4+"Season 7 of <em>Pokémon the Series</em> is now playing on Pokémon TV"+news_5+"Screenshots"+news_6;
+
+var zaermurgajocgvoehmnm = news_1+"/en/pokemon-tv-is-showing-a-collection-of-episodes-featuring-some-of-ashs-old-friends-returning"+news_2+"https://assets.pokemon.com/assets/cms2/img/watch-pokemon-tv/_tiles/stunts/old-friends/old-friends-169-en.png"+news_3+"29 January 2021"+news_4+"Pokémon TV is showing a collection of episodes featuring some of Ash's old friends returning."+news_5+"Images"+news_6;
