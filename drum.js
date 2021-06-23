639e694da17c29295b256dc0fc33502640bf5328
Shayak Roychowdhury <69388919+Shayak99@users.noreply.github.com>
2021-06-23 23:01:52 +0530

    initial commit

---
+++ drum.js
@@ -0,0 +1,10 @@
+// var len = document.querySelectorAll(".drum").length;
+// for (var i = 0; i < len; i++) {
+//   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+//     if ((this.style.color = "white")) this.style.color = "brown";
+//     else this.style.color = "white";
+//   });
+// }
+// document.addEventListener("keypress", function (event) {
+//   alert("Key was pressed");
+// });
