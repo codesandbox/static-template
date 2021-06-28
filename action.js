cc54fbaa1cdea26733fa27e17258edd61714a6a6
Andrea G <andreagrieco01@gmail.com>
2021-06-28 11:52:37 +0200

    commit

---
+++ action.js
@@ -0,0 +1,7 @@
+function do() {}
+function getData() {
+  let num1 = document.querySelector("#num1");
+  let num2 = document.querySelector("#num2");
+}
+function printResult() {}
+function action() {}
