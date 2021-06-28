3d3f370b0e1c15d3d89c8b346f0cf830a6c2669f
Andrea G <andreagrieco01@gmail.com>
2021-06-28 12:01:38 +0200

    mmh

--- action.js
+++ action.js
@@ -1,7 +1,33 @@
-function do() {}
-function getData() {
-  let num1 = document.querySelector("#num1");
-  let num2 = document.querySelector("#num2");
+let num1;
+let num2;
+let pari;
+
+function doStuff() {
+  let sum = num1 + num2;
+
+  if (sum / 2 == 0) {
+    pari = true;
+  } else {
+    pari = false;
+  }
 }
-function printResult() {}
-function action() {}
+function getData() {
+  num1 = document.querySelector("#num1");
+  num2 = document.querySelector("#num2");
+}
+function printResult(val) {
+  if (val == 1) {
+    console.log("ok");
+  } else {
+    console.log("oh sh1t");
+  }
+}
+function action() {
+  getData();
+  doStuff();
+  if (pari == true) {
+    printResult(1);
+  } else {
+    printResult(2);
+  }
+}
