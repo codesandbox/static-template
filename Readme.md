eb7ea907e2352f3ea69589a7b946f61f994d596b
wbabello <wbabello@yahoo.com>
2021-08-03 04:10:09 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Crown Movies portal:
+
+This is a platform (website) showing list of movies and Tv Prgrammes available to view by the public
+
+# Technology used:
+
+HTML,CSS, Javascript
+
+# Approach taken:
+
+User stories definition
+Wireframe design
+HTML Box building
+Visuals / Images gathering
+CSS Styling
+Javascripting
+
+# Funtionalities developed:
+
+Main page - showing company background information
+Movies Scehdule page - showing list of available movies to watch
+Recommendatiion section - showing key recommendations
+Contact us- ability to register and capture user information
+Direct linking to social medias
+
+# List of Known Issue:
+
+TBD
---
+++ schedule.js
@@ -0,0 +1,16 @@
+var movies = [
+  "Alive June 18th, 2021",
+  "AnneFrank - Parallel Stories, August 18th 2019",
+  "FriendButMarried	, September 5th,2018",
+  "100 Days of Solitude, May 23rd 2021",
+  "1898: Our Last Men in the Philippines , October 10th, 2020",
+  "#FriendButMarried 2 , June 15th, 2021",
+  "13TH: A Conversation with Oprah Winfrey & Ava DuVernay , March 8th, 2019",
+  "18 Presents , April 1st, 2021",
+  "1BR  February 2nd, 2020",
+  "100 Meters December 8th, 2021"
+];
+// document.querySelector(".schedule").innerHTML = movies[0];
+for (let i = 0; i < movies.length; i++) {
+  document.querySelector(".schedule").innerHTML += `<p>${movies[i]}</p>`;
+}
