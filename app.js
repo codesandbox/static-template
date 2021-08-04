4093b334b344f0c5371bbcd2d80e833396592244
CA-RA <88388708+CA-RA@users.noreply.github.com>
2021-08-04 11:13:35 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let Input = document.querySelector("#add");
+let btn = document.querySelector("#btn");
+let list = document.querySelector("#list");
+let li = document.getElementsByTagName("li");
+
+btn.addEventListener("click", () => {
+  let txt = Input.value;
+  if (txt === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = txt;
+    list.insertBefore(li, list.childNodes[0]);
+    Input.value = "";
+  }
+});
+
+list.addEventListener("click", (cancel) => {
+  if (cancel.target.TagName === "li") {
+    cancel.target.classlist.toggle("checked");
+  }
+});
