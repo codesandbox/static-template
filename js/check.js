4fab63365a7e5f04099be026453b1793e10b2d38
Lokki68 <81553708+Lokki68@users.noreply.github.com>
2021-08-11 07:36:09 +0200

    Onglet - Start

--- js/check.js
+++ js/check.js
@@ -1,1 +1,14 @@
-const icone = `<span>icone</span>`;
+const icone = `<span class="check"><i class="fas fa-check-square"></i></span>`;
+
+let tabli = document.querySelectorAll('ul li');
+
+for (li of tabli) {
+  li.addEventListener('click', function () {
+    this.classList.toggle('active');
+    if (this.classList.contains('active')) {
+      this.innerHTML = this.innerText + icone;
+    } else {
+      this.innerHTML = this.innerText;
+    }
+  });
+}
