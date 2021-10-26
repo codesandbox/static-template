609e2e30cdbae545d753d82e2974f5aaf4fd9807
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2021-10-26 16:30:58 +0100

    Css linked js started

---
+++ game.js
@@ -0,0 +1,7 @@
+function Bear() {
+  this.dBear = 100;
+  this.htmlElement = document.getElementById("bear");
+  this.id = this.htmlElement.id;
+  this.x = this.htmlElement.offsetLeft;
+  this.y = this.htmlElement.offsetTop;
+}
