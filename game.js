3c1e3a245f751a11082e3b87b7dcf9d66db5a617
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2021-10-26 16:39:32 +0100

    js update

--- game.js
+++ game.js
@@ -4,4 +4,15 @@ function Bear() {
   this.id = this.htmlElement.id;
   this.x = this.htmlElement.offsetLeft;
   this.y = this.htmlElement.offsetTop;
+
+  this.move = function (xDir, yDir) {
+    this.x += this.dBear * xDir;
+    this.y += this.dBear * yDir;
+  };
+
+  this.display = function () {
+    this.htmlElement.style.left = this.x + "px";
+    this.htmlElement.style.top = this.x + "px";
+    this.htmlElement.style.display = "absolute";
+  };
 }
