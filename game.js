3ec16b3f54dadf064feb3bb3de1c15923dbc9420
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2021-10-26 20:21:40 +0100

    problem_ bear undefined

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
+function start() {
+  // create bear
+
+  bear = new Bear();
+  // Add an event listener to the keypress event.
+
+  document.addEventListener("keydown", moveBear, false);
+}
+
 function Bear() {
   this.dBear = 100;
   this.htmlElement = document.getElementById("bear");
@@ -9,10 +18,30 @@ function Bear() {
     this.x += this.dBear * xDir;
     this.y += this.dBear * yDir;
   };
-
   this.display = function () {
     this.htmlElement.style.left = this.x + "px";
     this.htmlElement.style.top = this.x + "px";
     this.htmlElement.style.display = "absolute";
   };
+}
+//codes of the four keys
+// Handle keyboad events
+function moveBear(e) {
+  //codes of the four keys
+  const KEYUP = 38;
+  const KEYDOWN = 40;
+  const KEYLEFT = 37;
+  const KEYRIGHT = 39;
+  if (e.keyCode == KEYRIGHT) {
+    bear.move(1, 0);
+  } // right key
+  if (e.keyCode == KEYLEFT) {
+    bear.move(-1, 0);
+  } // left key
+  if (e.keyCode == KEYUP) {
+    bear.move(0, -1);
+  } // up key
+  if (e.keyCode == KEYDOWN) {
+    bear.move(0, 1);
+  } // down key
 }
