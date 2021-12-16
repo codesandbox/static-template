7d38ff7f07b0daa24c35a1125548672b45b8a8c6
thorebr <95279412+thorebr@users.noreply.github.com>
2021-12-16 17:11:13 +0100

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
