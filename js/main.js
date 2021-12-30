5bd9ca06c27c7b47b847547b88f6dd0a369f0f0e
otinomz <82697198+otinomz@users.noreply.github.com>
2021-12-30 09:12:04 +0100

    display names function created

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ const initApp = () => {
     clearSuggestions();
     // generate names
     const namesArray = generateNames();
-    console.log(namesArray);
-
-    // display names
+    // dispplay names
+    displayNames(namesArray);
   });
 };
 
@@ -17,28 +16,45 @@ document.addEventListener("DOMContentLoaded", initApp);
 
 const clearSuggestions = () => {
   const display = document.getElementById("suggestion-section");
-  if (!display.classList.contains("hiddden"))
-    display.classList.toggle("hidden");
-  const list = document.querySelector(".suggestion-secton ol");
+  if (!display.classList.contains("hidden")) display.classList.toggle("hidden");
+  const list = document.querySelector(".suggestion-section ol");
   list.innerHTML = "";
 };
 
 const generateNames = () => {
-  const randomNumberArray = [];
+  const randomNumArr = [];
   for (let i = 0; i < 4; ) {
     const randomNumber = Math.floor(Math.random() * 10);
-    if (randomNumberArray.includes(randomNumber)) continue;
-    randomNumberArray.push(randomNumber);
+    if (randomNumArr.includes(randomNumber)) continue;
+    randomNumArr.push(randomNumber);
     i++;
   }
-  const suggestion1 =
-    namesOne[randomNumberArray[0]] + namesTwo[randomNumberArray[3]];
-  const suggestion2 =
-    namesOne[randomNumberArray[1]] + namesTwo[randomNumberArray[0]];
-  const suggestion3 =
-    namesOne[randomNumberArray[2]] + namesTwo[randomNumberArray[2]];
-  const suggestion4 =
-    namesOne[randomNumberArray[3]] + namesTwo[randomNumberArray[1]];
+  const suggestion1 = namesOne[randomNumArr[0]] + namesTwo[randomNumArr[3]];
+  const suggestion2 = namesOne[randomNumArr[1]] + namesTwo[randomNumArr[0]];
+  const suggestion3 = namesOne[randomNumArr[2]] + namesTwo[randomNumArr[2]];
+  const suggestion4 = namesOne[randomNumArr[3]] + namesTwo[randomNumArr[1]];
 
   return [suggestion1, suggestion2, suggestion3, suggestion4];
+};
+
+const displayNames = (namesArray) => {
+  const list = document.querySelector(".suggestion-section ol");
+  const rawFirstName = document.getElementById("submitSection-textInput").value;
+  const firstName = sanitizeInput(rawFirstName);
+  namesArray.forEach((name) => {
+    list.innerHTML += `<li>
+            <a href="https://youtube.com/${name}" target="_blank">${name}</a></li>`;
+    list.innerHTML += `<ul>
+            <li><a href="https://youtube.com/${firstName}s${name}" target="_blank">${firstName}s${name}</a></li> 
+            <li><a href="https://youtube.com/${name}With${firstName}" target="_blank">${name}With${firstName}</a></li>
+            </ul>`;
+  });
+  const display = document.getElementById("suggestion-section");
+  if (display.classList.contains("hidden")) display.classList.toggle("hidden");
+};
+
+const sanitizeInput = (inputValue) => {
+  const div = document.createElement("div");
+  div.textContent = inputValue;
+  return div.innerHTML;
 };
