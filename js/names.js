c2e1c3af2e02d5f54b9f84b22ada9be404add41b
otinomz <82697198+otinomz@users.noreply.github.com>
2021-12-30 07:31:03 +0100

    initial commit

---
+++ js/names.js
@@ -0,0 +1,25 @@
+export const namesOne = {
+  0: "code",
+  1: "Learning",
+  2: "Coding",
+  3: "Program",
+  4: "Coder",
+  5: "Programming",
+  6: "Dev",
+  7: "WebDev",
+  8: "Web",
+  9: "Tech"
+};
+
+export const namesTwo = {
+  0: "Life",
+  1: "MadeEasy",
+  2: "Programmer",
+  3: "Simplified",
+  4: "Development",
+  5: "World",
+  6: "Media",
+  7: "Developer",
+  8: "Stacker",
+  9: "WebDev"
+};
