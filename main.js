34321cca63479ddddb1f6495945b5ac0494a63fa
Quinn135 <59322843+Quinn135@users.noreply.github.com>
2022-01-28 18:49:04 -0600

    hah

--- main.js
+++ main.js
@@ -1,4 +1,59 @@
+var url = new URL(window.location);
+var game = url.searchParams.get("game");
+
+if (game === "other") {
+  var questions = [
+    ["I dont know?", "I do know", "You know", "We all know", "I know", 1],
+    ["What is your favorite color", "Jack", "Purple", "Brick", "Space", 4]
+  ];
+} else if (game === "numbers") {
+  var questions = [
+    ["Which one is a prime number?", "2", "4", "6", "9", 1],
+    ["Pick", "42", "43", "44", "47", 1]
+  ];
+} else if (game === "farts") {
+  var questions = [
+    [
+      "which one of these phrases is the most anoying",
+      "deez nuts",
+      "AYYYOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+      "YOUR A DUMB BOZZZZZOZOZOZOZOZOZOZOZZOZO",
+      "this is correct please trust me PLEEESESESESESESESES PLEASE PLEASE PLEASE PLEASE I REALLY NEED YOU TO TRUST OR I WILL UGHHHHHH COME TO YOUR HOUSE OR SOMETHING JUSTT PLEAEASAASESEAEA",
+      4
+    ],
+    [
+      "what is the best instrument",
+      "fart",
+      "piano",
+      "guitar",
+      "HAHHAHAHHAHAHAH",
+      4
+    ],
+    [
+      "What is the best way to fart",
+      "shart",
+      "fart and sneeze",
+      "shart while farting and cough and sneeze and sing happy birthday at the same time while also singing bohemeideim rapsody",
+      "throw up while farting",
+      3
+    ]
+  ];
+} else {
+  var questions = [
+    [
+      "Something went wrong I guess... <a href='play.html'>Back</a>",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ]
+  ];
+}
+
+/*
 var questions = [
+  ["Which one is a prime number?", "2", "4", "6", "9", 1],
   ["I dont know?", "I do know", "You know", "We all know", "I know", 1],
   [
     "which one of these phrases is the most anoying",
@@ -6,7 +61,7 @@ var questions = [
     "AYYYOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
     "YOUR A DUMB BOZZZZZOZOZOZOZOZOZOZOZZOZO",
     "this is correct please trust me PLEEESESESESESESESES PLEASE PLEASE PLEASE PLEASE I REALLY NEED YOU TO TRUST OR I WILL UGHHHHHH COME TO YOUR HOUSE OR SOMETHING JUSTT PLEAEASAASESEAEA",
-    0
+    4
   ],
   ["Pick", "42", "43", "44", "47", 1],
   ["What is your favorite color", "Jack", "Purple", "Brick", "Space", 4],
@@ -26,13 +81,16 @@ var questions = [
     "throw up while farting",
     3
   ]
-];
+*/
 
 var cor = 0;
 var problem = 1;
 
+window.onload = function () {
+  document.getElementById("level").innerHTML = problem + "/" + questions.length;
+};
+
 function check(value) {
-  alert(Number(value.value) + "     " + Number(questions[problem - 1][5]));
   if (Number(questions[problem - 1][5]) === Number(value.value)) {
     correct();
     problem++;
@@ -50,7 +108,7 @@ function set(problem) {
     document.getElementById("f").style.display = "flex";
     document.getElementById("f").innerHTML = `YOU FINISHED.<br>${cor} / ${
       problem - 1
-    } <button onclick="window.location.reload()">Retry</button>`;
+    } <button onclick="window.location.reload()">Retry</button> <a href="/index.html"><button>Back</button></a>`;
   } else {
     document.getElementById("title").innerHTML = questions[problem - 1][0];
     if (questions[problem - 1][1] === "") {
@@ -92,5 +150,12 @@ function hide() {
   window.setTimeout(function () {
     document.getElementById("c").style.display = "none";
     document.getElementById("i").style.display = "none";
+    if (problem > questions.length) {
+      document.getElementById("level").innerHTML =
+        Number(problem) - 1 + "/" + questions.length;
+    } else {
+      document.getElementById("level").innerHTML =
+        problem + "/" + questions.length;
+    }
   }, 2000);
 }
