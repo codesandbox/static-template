711c8d53a2c58659bba16b8727d2c5d8c4cdc96e
Surabhi Gupta <96222130+Surrbhi18@users.noreply.github.com>
2022-07-07 14:21:34 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    des:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exmagnam veritatis! Quasi temporibus pariatur esse cupiditate",
+    image: "images/slider 2.png"
+  },
+  {
+    name: "Loki",
+    des:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exmagnam veritatis! Quasi temporibus pariatur esse cupiditate",
+    image: "images/slider 1.png"
+  },
+  {
+    name: "Wanda vision",
+    des:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exmagnam veritatis! Quasi temporibus pariatur esse cupiditate",
+    image: "images/slider 3.png"
+  },
+  {
+    name: "Raya and the last dragon",
+    des:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exmagnam veritatis! Quasi temporibus pariatur esse cupiditate",
+    image: "images/slider 4.png"
+  },
+  {
+    name: "Luca",
+    des:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exmagnam veritatis! Quasi temporibus pariatur esse cupiditate",
+    image: "images/slider 5.png"
+  }
+];
