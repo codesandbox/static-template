b959b40809dfaa8d920a52852168ecc18a43dad9
amolpalwe2002 <103014731+amolpalwe2002@users.noreply.github.com>
2022-07-16 01:57:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+// // alert("Hello World");
+// console.log("Hello World");
+// let name = prompt("Enter your name:");
+// console.log(name);
+
+// "Amol Nilay"
+// 'VIT'
+
+// Numbers
+// 234
+// 23.5
+
+// boolean
+// true
+// false
+
+// // document.write("Hello Guys wake up");
+// document.write("<h1>Good Morning</h1>");
+
+// single comment
+/* muti line 
+comments
+ */
+
+// let age = prompt("Enter your age:");
+// let ans;
+
+// if (age >= 18) {
+//   ans = "Adult";
+// } else {
+//   ans = "Minor";
+// }
+
+// ans = age >= 18 ? "Adult" : "Minor";
+// console.log(ans);
+
+// const arr1 = [1, 2, 3];
+// // const arr2 = ["a", "b", "c"];
+// // console.log(arr2);
+// const arr2 = [...arr1, "a", "b", "c"];
+// console.log(arr2);
+
+// const student = {
+//   name: "Amol",
+//   college: "Vit",
+//   marks: {
+//     EM: 50,
+//     MAR: 40,
+//     SE: 45
+//   }
+// };
+
+// console.log(student.marks);
+// console.log(student["marks"]["MAR"]);
+
+// let {
+//   name,
+//   marks: { EM, MAR },
+//   college
+// } = student;
+
+// console.log(EM);
+
+// function greet() {
+//   return "Hello Guys";
+// }
+// let x = greet();
+// alert(x);
+
+// hello = function(){
+//   return "Hello World"
+// };
+
+//  hello = () => "hello World";
+
+// hello();
