9666fbb8baf63dd2b2acfb396819ae882bd9df24
Jack McGonigle <93732320+jackmcgonigle@users.noreply.github.com>
2022-08-09 17:30:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//hey boyo
+
+console.log("hello world!");
+
+//numbers
+
+console.log(42);
+console.log(42.781);
+
+console.log(5 + 2);
+console.log(10 - 6);
+console.log(12 / 2);
+console.log(3 * 6);
+
+//booleans
+console.log(true);
+console.log(false);
+
+//
