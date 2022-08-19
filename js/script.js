580be0c4f60ca234e0ad994b829f294160d96cd1
raydel8512 <69324125+raydel8512@users.noreply.github.com>
2022-08-19 12:09:49 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function prueba() {
+  alert("mensaje del boton1");
+}
+/*window.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".nav li a").forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      alert("Aqui iriamos a la pagina correspondiente");
+    });
+  });
+});*/
+
+window.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector("#robbu-whatsapp-button")
+    .addEventListener("click", function () {
+      alert("Aqui se genera la llamada a Whatsapp");
+    });
+});
