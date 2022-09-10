a13c8c5817cb47e8768cbc85c4cafd1fc808b226
Sukhvant Singh <49615949+SSukhvant@users.noreply.github.com>
2022-09-11 00:22:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
