4a38c34a75e1c1f206f121721fd3e9f9e8a0e35a
Selfrep <44550038+self-rep@users.noreply.github.com>
2022-10-19 09:01:55 +0100

    c99 geo ip update

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let API_KEY = "";
+let API_URL = `https://api.c99.nl/geoip?key=${API_KEY}&host=`;
+
 function TryIP() {
   let isOk = true;
   let ip = document.getElementById("ip").value;
@@ -11,7 +14,7 @@ function TryIP() {
         // check each val is number
         let parsed_int = parseInt(splitted[i]);
         if (!isNaN(parsed_int)) {
-          if (parsed_int <= 255 && parsed_int > 0) {
+          if (parsed_int <= 255 && parsed_int >= 0) {
             // is correct
             console.log("is correct -> ", parsed_int);
             continue;
@@ -57,4 +60,47 @@ function AlertInvalid(ip, reason) {
 
 function AlertSuccess(ip) {
   swal("Success!", `Successfully Validated ipaddress -> ${ip}`, "success");
+}
+
+async function Lookup() {
+  let ip = document.getElementById("ip").value;
+
+  if (ip.length !== 0) {
+    // check if ip is valid
+    if (isValidIpAddress(ip)) {
+      fetch(`${API_URL}${ip}&json`)
+        .then((x) => x.json())
+        .then((r) => {
+          if (r?.success) {
+            // `?` quicker way of saying if r.success !=== null
+            swal("Success!", `Lookup Data \n ip: ${ip}\n`, "success");
+          } else {
+            alert("something went wrong! -> ", r?.error);
+          }
+        });
+    } else {
+      console.log("is not valid");
+    }
+  } else {
+    AlertInvalid(ip, "No Input");
+  }
+}
+
+function isValidIpAddress(ip) {
+  let splitted = ip.split(".");
+
+  if (splitted.length === 4) {
+    for (let i = 0; i < splitted.length; i++) {
+      // parse int
+      let parsed_int = parseInt(splitted[i]);
+      if (isNaN(parsed_int)) {
+        return false;
+      }
+
+      if (parsed_int > 255 || parsed_int < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
