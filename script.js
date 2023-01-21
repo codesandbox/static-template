835fc3cccc6453bfe0df59429d76d18b460a307c
pandoedo <122695573+Pandoedo1105@users.noreply.github.com>
2023-01-22 00:23:52 +0700

    ini berubah

--- script.js
+++ script.js
@@ -75,3 +75,30 @@
 // sectionB.replaceChild(h2Baru, p4);
 
 // DOM Events
+// EventHandler
+
+const p3 = document.querySelector(".p3");
+
+function ubahWarnaP2() {
+  p2.style.backgroundColor = "salmon";
+}
+
+function ubahWarnaP3() {
+  p3.style.backgroundColor = "orange";
+}
+
+const p2 = document.querySelector(".p2");
+p2.onclick = ubahWarnaP2;
+
+// ===========================================
+const p4 = document.querySelector("section#b p");
+
+p4.addEventListener("click", function () {
+  const ul = document.querySelector("section#b ul");
+
+  const liBaru = document.createElement("li");
+  const liTeks = document.createTextNode("Item Baru");
+
+  liBaru.appendChild(liTeks);
+  ul.appendChild(liBaru);
+});
