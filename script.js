ea838e47ab0642a1f9908b52e21b3226f4ef5b78
pandoedo <122695573+Pandoedo1105@users.noreply.github.com>
2023-01-22 00:42:49 +0700

    rubah lagi

--- script.js
+++ script.js
@@ -75,30 +75,47 @@
 // sectionB.replaceChild(h2Baru, p4);
 
 // DOM Events
-// EventHandler
+// // EventHandler
 
-const p3 = document.querySelector(".p3");
+// const p3 = document.querySelector(".p3");
 
-function ubahWarnaP2() {
-  p2.style.backgroundColor = "salmon";
-}
+// function ubahWarnaP2() {
+//   p2.style.backgroundColor = "salmon";
+// }
 
-function ubahWarnaP3() {
-  p3.style.backgroundColor = "orange";
-}
+// function ubahWarnaP3() {
+//   p3.style.backgroundColor = "orange";
+// }
 
-const p2 = document.querySelector(".p2");
-p2.onclick = ubahWarnaP2;
+// const p2 = document.querySelector(".p2");
+// p2.onclick = ubahWarnaP2;
 
 // ===========================================
-const p4 = document.querySelector("section#b p");
+// const p4 = document.querySelector("section#b p");
 
-p4.addEventListener("click", function () {
-  const ul = document.querySelector("section#b ul");
+// p4.addEventListener("click", function () {
+//   const ul = document.querySelector("section#b ul");
 
-  const liBaru = document.createElement("li");
-  const liTeks = document.createTextNode("Item Baru");
+//   const liBaru = document.createElement("li");
+//   const liTeks = document.createTextNode("Item Baru");
 
-  liBaru.appendChild(liTeks);
-  ul.appendChild(liBaru);
+//   liBaru.appendChild(liTeks);
+//   ul.appendChild(liBaru);
+// });
+
+// Event Handler
+const p3 = document.querySelector(".p3");
+// p3.onclick = function () {
+//   p3.style.backgroundColor = "navy";
+// };
+
+// p3.onclick = function () {
+//   p3.style.color = "red";
+// };
+
+p3.addEventListener("mouseenter", function () {
+  p3.style.backgroundColor = "navy";
+});
+p3.addEventListener("mouseleave", function () {
+  p3.style.backgroundColor = "salmon";
 });
