7bf5a74db0ae917888df1af52b08d79f4baa2903
Sayan424 <118917167+Sayan424@users.noreply.github.com>
2023-03-27 01:20:19 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    des:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider2.png"
+  },
+  {
+    name: "loki",
+    des:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider1.png"
+  },
+  {
+    name: "wanda vision",
+    des:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider3.png"
+  },
+  {
+    name: "raya and the last dragon",
+    des:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider4.png"
+  },
+  {
+    name: "luca",
+    des:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider5.png"
+  }
+];
