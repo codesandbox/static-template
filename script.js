079dfe6849403add5dd0f83e4ec4d05c98a6e25f
Ghanshyam Prajapati <63035436+Ghanshyam89@users.noreply.github.com>
2023-03-31 09:51:34 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+  const sidebar = document.querySelector(".sidebar");
+  const closeBtn = document.querySelector("#btn");
+  const searchBtn = document.querySelector(".bx-search");
+
+  closeBtn.addEventListener("click", function () {
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+  });
+
+  searchBtn.addEventListener("click", function () {
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+  });
+
+  function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+      closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+      closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+  }
+};
