eec0b7492d569565a37442e5f20fe4c2e27449f3
isabela-penam <123768432+isabela-penam@users.noreply.github.com>
2023-04-14 14:01:02 -0500

    acumulativa

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
-document.addEventListener("DOMContentLoaded", =>{
-  const cardAdj= [
-  {
-    name: "c",
-    img: "images/hello_kitty_1[1].jpg"
-    img: "images/hello_kitty_2[2].jpg"
-    img: "images/hello_kitty_3[3].jpg"
-    img: "images/hello_kitty_4[4].jpg"
-    img: "images/hello_kitty_5[5].jpg"
-    img: "images/hello_kitty_6[6].jpg"
-
-  },
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+ 
+    {
+      name: "1",
+      img: "images/hello_kitty_1[1].jpg"
+    },
+    {
+      name: "2",
+      img: "images/hello_kitty_2[2].jpg"
+    },
+    {
+      name:"3",
+      img: "images/hello_kitty_3[3].jpg"
+    },
+    {
+      name:"4",
+      img: "images/hello_kitty_4[4].jpg"
+    },
+    {
+      name:"5",
+      img: "images/hello_kitty_5[5].jpg"
+    },
+    {
+      name:"6",
+      img: "images/hello_kitty_6[6].jpg"
+  
+    },
+    {
+      name: "1",
+      img: "images/hello_kitty_1[1].jpg"
+    },
+    {
+      name: "2",
+      img: "images/hello_kitty_2[2].jpg"
+    },
+    {
+      name:"3",
+      img: "images/hello_kitty_3[3].jpg"
+    },
+    {
+      name:"4",
+      img: "images/hello_kitty_4[4].jpg"
+    },
+    {
+      name:"5",
+      img: "images/hello_kitty_5[5].jpg"
+    },
+    {
+      name:"6",
+      img: "images/hello_kitty_6[6].jpg"
+  
+    },
  
 ];
 
@@ -46,6 +87,10 @@ for (let i= 0; 1 < cardAdj.lenght; i++) {
 
 function voltearCarta(){
   var cartasId = this.getAttribute("data-id");
-  cartasEscogidas.push(cardAdj[cartasId].name)
+  cartasEscogidas.push(cardAdj[cartasId].name);
+  cartasEscogidasId.push(cardId);
+  this.setAttribute("scr",cardAdj[cardId].img);
+  if (cartasEscogidas.lenght === 2) {
+    setTimeout(verificarpareja, 1000);
+  }
 }
-);
