113e66fbebff4881a367d295bb106b8001210c14
taylor-kern95 <135791249+taylor-kern95@users.noreply.github.com>
2023-07-17 22:36:42 -0500

    initial commit

---
+++ js/gallery.js
@@ -0,0 +1,8 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+  //console.log("ready!");
+
+  $("#imed1316-gallery").nanogallery2({
+    //nanogallery
+  }); //nanogallery
+}); //document ready
