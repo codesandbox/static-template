a7ff34e7134fd4ea647003dd07453b779d5969e6
Social Inviter <socialinviters@gmail.com>
2023-10-06 09:59:14 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var div = document.createElement("div");
+div.innerHTML = `<div class="nav-link">
+<h4>Demo</h4>
+<a href="index.html">Contact Importer</a>
+<a href="sociallogin.html">Social Login</a>
+<a href="socialshare.html">Social Share</a>
+<a href="socialpost.html">Social Post</a>
+</div>
+<hr />`;
+// document.querySelector('body').prepend(div);
