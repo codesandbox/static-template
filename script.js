ca5615f1a31066aa82c5457f3798d4bf8504bd5c
Letícia Schneider <leticia.schneider@gmail.com>
2023-11-05 18:07:47 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("circle1").addEventListener("click", function () {
+  document.getElementById("imagem").src = "./resourses/macbook_coral.png";
+  document
+    .getElementById("btn_comprar")
+    .setAttribute("href", "https://pay.hotmart.com/P87821837V");
+});
+
+document.getElementById("circle2").addEventListener("click", function () {
+  document.getElementById("imagem").src = "./resourses/macbook_blue.png";
+  document
+    .getElementById("btn_comprar")
+    .setAttribute("href", "https://pay.hotmart.com/M87821775W");
+});
+
+document.getElementById("circle3").addEventListener("click", function () {
+  document.getElementById("imagem").src = "./resourses/macbook_purple.png";
+  document
+    .getElementById("btn_comprar")
+    .setAttribute("href", "https://pay.hotmart.com/O87821863Y");
+});
+
+document.getElementById("circle4").addEventListener("click", function () {
+  document.getElementById("imagem").src = "./resourses/macbook_gray.png";
+  document
+    .getElementById("btn_comprar")
+    .setAttribute("href", "https://pay.hotmart.com/F87821713Q");
+});
