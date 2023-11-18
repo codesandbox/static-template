743c64bda69b1ccf752f4356cecf99a791d05088
disiol <deoniisii@gmail.com>
2023-11-19 00:48:58 +0200

    217-graph

---
+++ scripts/canvasFigures.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
+context.fillStyle = "red"; // Встановіть колір заливки
+context.fillRect(50, 50, 200, 100);
+ // Малюємо контур прямокутника
+// Малюємо дугу (коло)
+context.fillStyle = "blu"; // Встановіть колір заливки
+context.beginPath();
+context.arc(50, 200, 50, 0, Math.PI * 2); // Координати центра, радіус, початковий та кінцевий кути
+context.strokeStyle = "blue"; // Встановлюємо колір контура
+context.stroke(); // Відображаємо контур
+
