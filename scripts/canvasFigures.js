f8f24041fe8ccb28b5685afd69d2fef67768fd2d
disiol <deoniisii@gmail.com>
2023-11-19 09:38:30 +0200

    add context.fill(); context.closePath();

--- scripts/canvasFigures.js
+++ scripts/canvasFigures.js
@@ -4,9 +4,10 @@ context.fillStyle = "red"; // Встановіть колір заливки
 context.fillRect(50, 50, 200, 100);
  // Малюємо контур прямокутника
 // Малюємо дугу (коло)
-context.fillStyle = "blue"; // Встановіть колір заливки
 context.beginPath();
+context.fillStyle = "blue"; // Встановіть колір заливки
 context.arc(50, 200, 50, 0, Math.PI * 2); // Координати центра, радіус, початковий та кінцевий кути
+context.fill();
 context.strokeStyle = "blue"; // Встановлюємо колір контура
-context.stroke(); // Відображаємо контур
-
+context.stroke();// Відображаємо контур
+context.closePath();
